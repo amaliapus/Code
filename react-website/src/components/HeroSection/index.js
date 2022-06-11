5d9ff4faa64a5c75046a89b25cf775ffd470e2e4
amaliapus <amaliapus.9d@gmail.com>
2022-06-11 22:47:05 +0700

    add HeroSection

--- react-website/src/components/HeroSection/index.js
+++ react-website/src/components/HeroSection/index.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Video from '../../videos/video.mp4';
-import { HeroContainer, HeroBg, VideoBg, HeroH1, HeroP, HeroBtnWrapper, ArrowForward, ArrowRight } from './HeroElements';
+import { HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1, HeroP, HeroBtnWrapper, ArrowForward, ArrowRight } from './HeroElements';
 
 const HeroSection = () => {
+    const [hoter, setHover] = useState(false)
+
+    const onHover = () => {
+        setHover(!hover)
+    }
     return (
         <HeroContainer>
             <HeroBg>
@@ -14,7 +19,7 @@ const HeroSection = () => {
                     Sign up for a new account today and receive $250 in credit towards your next payment.
                 </HeroP>
                 <HeroBtnWrapper>
-                    <Button to="signup">
+                    <Button to='signup' onMouseEnter={onHover} onMouseLeave={onHover}>
                         Get started {hover ? <ArrowForward /> : <ArrowRight />}
                     </Button>
                 </HeroBtnWrapper>
