840514238cbf5a9276378742184fcf90289f66c8
amaliapus <amaliapus.9d@gmail.com>
2022-06-13 23:14:02 +0700

    add InfoElements pages'

--- react-website/src/pages/index.js
+++ react-website/src/pages/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Sidebar from '../components/Sidebar';
+import InfoSection from '../components/InfoSection';
 import Navbar from '../components/Navbar';
 import HeroSection from '../components/HeroSection';
 
@@ -15,6 +16,7 @@ const Home = () => {
             <Sidebar isOpen={isOpen} toggle={toggle} />
             <Navbar toggle={toggle} />
             <HeroSection />
+            <InfoSection />
         </>
     );
 };
