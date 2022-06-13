5fb25a4a0b4da7b6ecc5e9ae645141af97fe8b47
amaliapus <amaliapus.9d@gmail.com>
2022-06-13 23:37:52 +0700

    add data.js

---
+++ react-website/src/components/InfoSection/Data.js
@@ -0,0 +1,17 @@
+import { toBeRequired } from "@testing-library/jest-dom/dist/matchers";
+
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Premium Bank',
+    headline: 'Unlimited Transactions with zero fees',
+    description: 'Get access to our exclusive app that allows you to send unlimited transection without gettiong charged any fees.',
+    buttonLabel: 'Get Started',
+    img: require('../../images/svg-1.svg'),
+    alt: 'Car',
+    dark: true,
+    primary: true,
+    dataText: false
+};
