e7bb4c611bcb3d5ee0cb3060c52795261d63d6d3
amaliapus <amaliapus.9d@gmail.com>
2022-06-19 05:41:10 +0700

    finishing

--- react-website/src/components/HeroSection/index.js
+++ react-website/src/components/HeroSection/index.js
@@ -10,7 +10,7 @@ const HeroSection = () => {
         setHover(!hover)
     }
     return (
-        <HeroContainer>
+        <HeroContainer id='home'>
             <HeroBg>
                 <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
             </HeroBg>
@@ -20,9 +20,16 @@ const HeroSection = () => {
                     Sign up for a new account today and receive $250 in credit towards your next payment.
                 </HeroP>
                 <HeroBtnWrapper>
-                    <Button to='signup' onMouseEnter={onHover} onMouseLeave={onHover}
+                    <Button to='signup'
+                        onMouseEnter={onHover}
+                        onMouseLeave={onHover}
                         primary='true'
                         dark='true'
+                        smooth={true}
+                        duration={500}
+                        spy={true}
+                        exact='true'
+                        offset={-80}
                     >
                         Get started {hover ? <ArrowForward /> : <ArrowRight />}
                     </Button>
--- react-website/src/components/Signin/SigninElements.js
+++ react-website/src/components/Signin/SigninElements.js
@@ -2,19 +2,19 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 export const Container = styled.div`
- min-height: 692px;
- position: fixed;
- bottom; 0;
- left: 0;
- right: 0;
- top: 0;
- z-index: 0;
- overflow: hidden;
- background: linear-gradient(
-    108deg,
-    rgba(1, 147, 86, 1) 0%,
-    rgba(84, 150, 157, 1) 100%
- );
+    min-height: 692px;
+    position: fixed;
+    bottom; 0;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 0;
+    overflow: hidden;
+    background: linear-gradient(
+        108deg,
+        rgba(1, 147, 86, 1) 0%,
+        rgba(84, 150, 157, 1) 100%
+    );
 `;
 
 export const FormWrap = styled.div`
@@ -35,6 +35,7 @@ export const Icon = styled(Link)`
     color: #fff;
     top-weight: 700;
     font-size: 32px;
+    font-weight: bold;
 
     @media screen and (max-width: 480px) {
         margin-left: 16px;
