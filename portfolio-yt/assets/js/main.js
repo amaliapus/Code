2330b4ebc574d20b6778a107ead5b9e64f2cc3de
amaliapus <amaliapus.9d@gmail.com>
2022-06-24 23:53:17 +0700

    until 58:58

--- portfolio-yt/assets/js/main.js
+++ portfolio-yt/assets/js/main.js
@@ -1,18 +1,18 @@
-// MENU SHOW Y HIDDEN
+/*==================== MENU SHOW Y HIDDEN ====================*/
 const navMenu = document.getElementById('nav-menu'),
     navToggle = document.getElementById('nav-toggle'),
     navClose = document.getElementById('nav-close')
 
-// MENU SHOW
-// Validate if constant exists
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
 if (navToggle) {
     navToggle.addEventListener('click', () => {
-        navMenu.classList.add(show - menu)
+        navMenu.classList.add('show-menu')
     })
 }
 
-// MENU HIDDEN
-// Validate if constant exists
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
 if (navClose) {
     navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu')
@@ -20,7 +20,7 @@ if (navClose) {
 }
 
 
-// REMOVE MENU MOBILE
+/*==================== REMOVE MENU MOBILE ====================*/
 const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction() {
@@ -29,3 +29,46 @@ function linkAction() {
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+/*==================== ACCORDION SKILLS ====================*/
+const skillsContent = document.getElementsByClassName('skills__content'),
+    skillsHeader = document.querySelectorAll('.skills__header')
+
+function toggleSkills() {
+    let itemClass = this.parentNode.className
+
+    for (i = 0; i < skillsContent.length; i++) {
+        skillsContent[i].className = 'skills__content skills__close'
+    }
+    if (itemClass === 'skills__content skills__close') {
+        this.parentNode.className = 'skills__content skills__open'
+    }
+}
+
+skillsHeader.forEach((el) => {
+    el.addEventListener('click', toggleSkills)
+})
+
+/*==================== QUALIFICATION TABS ====================*/
+
+
+/*==================== SERVICES MODAL ====================*/
+
+
+/*==================== PORTFOLIO SWIPER  ====================*/
+
+
+/*==================== TESTIMONIAL ====================*/
+
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/
+
+
+/*==================== SHOW SCROLL UP ====================*/
+
+
+/*==================== DARK LIGHT THEME ====================*/
