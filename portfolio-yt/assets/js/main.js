ba51df6a5267369dfe6ae35b469d1e3f7321f9b9
amaliapus <amaliapus.9d@gmail.com>
2022-07-03 06:35:49 +0700

    finishing the portfolio section

--- portfolio-yt/assets/js/main.js
+++ portfolio-yt/assets/js/main.js
@@ -94,7 +94,19 @@ modalCloses.forEach((modalClose) => {
 })
 
 /*==================== PORTFOLIO SWIPER  ====================*/
+var swiper = new Swiper(".mySwiper", {
+    cssMode: true,
+    loop: true,
 
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
 
 /*==================== TESTIMONIAL ====================*/
 
