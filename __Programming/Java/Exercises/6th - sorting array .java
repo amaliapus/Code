a1b6514ce480f135a98a0490afee289601f7d6b2
amaliapus <amaliapus.9d@gmail.com>
2022-07-14 17:40:16 +0700

    sorting array java

---
+++ __Programming/Java/Exercises/6th - sorting array .java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+        int  a[] = {8,3,19,26,12,1,65,14};
+        int i = a.length;
+        
+        Arrays.sort(a);
+        
+        System.out.println("The 2nd smallest: " +a[1]);
+    }
+}
